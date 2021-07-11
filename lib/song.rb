b75b2c981ba702e4910c9c347ad62cf1b2845e82
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-11 11:15:01 -0400

    Completed - add official soln. as an alternative to the original working soln. already committed.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,8 @@
 # Completed with help from the official solution on the 
 # first method.
+#
+# Differences between the original working solution and 
+# the official solution are noted.
 
 class Song
   attr_accessor :name, :artist_name
@@ -14,6 +17,7 @@ class Song
     self
   end
 
+  # inspired by the official solution
   def self.create
     song = Song.new
     song.save
@@ -34,21 +38,8 @@ class Song
     self.all.find{|et| et.name == name}
   end
 
-  def self.find_or_create_by_name(name)
-    song = self.find_by_name(name)
-    song ? song : self.create_by_name(name)
-  end
-
   def self.alphabetical
     self.all.sort_by!{|et| et.name}
-  end
-
-  def self.new_from_filename(filename)
-    artist, suffix = filename.split(" - ")
-    name = suffix[0..-5]
-    song = self.new_by_name(name)
-    song.artist_name = artist
-    song
   end
 
   def self.create_from_filename(name)
@@ -58,6 +49,36 @@ class Song
 
   def self.destroy_all
     self.all.clear
+  end
+
+  def self.find_or_create_by_name_0(name)
+    song = self.find_by_name(name)
+    song ? song : self.create_by_name(name)
+  end
+
+  # official solution
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.new_from_filename_0(filename)
+    artist, suffix = filename.split(" - ")
+    name = suffix[0..-5]
+    song = self.new_by_name(name)
+    song.artist_name = artist
+    song
+  end
+
+  # official solution
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
   end
 
 end
